c4e4c7f8d20a633f99aae2e5de15d8d3480ad15a
nakashima <nakashimayusuke@team-lab.com>
2014-10-20 10:57:40 +0900

    Add support for multi-account and animated-gif

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 @zorrilla\_氏作の[Twitpic API to twitter media upload gateway](http://lanieve.jp/picgw/)を見て作った，おそらく同じ動作をするプログラム
 
 ## 使い方
-`config.ini.example`を`config.py`という名前でコピーしてoauthの設定書き換えて`yoru_proxy.py`を走らせる．
+`config.ini.example`を`config.ini`という名前でコピーしてoauthと各ユーザの設定を書き換えて`yoru_proxy.py`を走らせる．
 
 ただし，Python3じゃないと動かない．
 
 夜フクロウの設定は`config.ini`のserverの項目いじってなければ`http://localhost:5000/`で良い．
+
+### config.iniについて追記
+`oauth`と`server`セクション以外のセクション名はアカウントの*screen\_name*に設定する．
+
+各ユーザのaccess\_token, access\_token\_secretはexampleのように各ユーザのセクション内に配置する．
 
 ## 依存パッケージ
 - [Flask](https://pypi.python.org/pypi/Flask/)
--- yoru_proxy.py
+++ yoru_proxy.py
@@ -6,20 +6,26 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-oauth_conf = config['oauth']
-server_conf = config['server']
+oauth_conf = dict(config['oauth'])
+server_conf = dict(config['server'])
+config.pop('oauth')
+config.pop('server')
 
-access_token = oauth_conf['access_token']
-access_token_secret = oauth_conf['access_token_secret']
 consumer_key = oauth_conf['consumer_key']
 consumer_secret = oauth_conf['consumer_secret']
 
+clients = {}
+for section in config.sections():
+    atoken = config[section]['access_token']
+    asecret = config[section]['access_token_secret']
+    clients[section.lower()] = \
+        Twitter(auth=(OAuth(atoken, asecret, consumer_key, consumer_secret)))
+
 app = Flask(__name__)
-twitter = Twitter(auth=OAuth(access_token, access_token_secret,
-                             consumer_key, consumer_secret))
 
 
-def update_with_media(status, media):
+def update_with_media(client, status, media):
+    twitter = clients[client.lower()]
     return twitter.statuses.update_with_media(**{'status': status,
                                                  'media[]': media})
 
@@ -27,14 +33,17 @@ def update_with_media(status, media):
 @app.route('/', methods=['POST'])
 def upload():
     msg = request.form['message']
+    user = request.form['username']
     img = request.files['media'].stream.read()
-    api_response = update_with_media(msg, img)
-    image_url = api_response['extended_entities']['media'][0]['display_url']
+    api_response = update_with_media(user, msg, img)
+    if 'media' in api_response['entities']:
+        image_url = api_response['entities']['media'][0]['display_url']
+    else:
+        image_url = api_response['entities']['urls'][-1]['display_url']
     xml = render_template('result.xml', image_url=image_url)
     response = Response(xml, mimetype='application/xml')
     return response
 
 if __name__ == '__main__':
-    server_conf = dict(server_conf)
     server_conf['port'] = int(server_conf.get('port', '5000'))
     app.run(**server_conf)
